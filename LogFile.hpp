22cc3c8788412306ba24e994ea36f4cea3cb9a3b
liuzf1986 <liuzhaofeng@veclink.com>
2015-02-02 19:09:37 +0800

    imply log file

--- LogFile.hpp
+++ LogFile.hpp
@@ -3,5 +3,14 @@
 #include <unistd.h>
 
 class LogFile {
-  
+ public:
+  LogFile(const char* baseName);
+  ~LogFile();
+  void applyFileName(const char* fileName);
+  void recreateFD();
+ private:
+  char _path[256];
+  int _fd;
 };
+
+
