01c5dcaa92f9d39230cf4d18d3978fb1ca4fdfc2
liuzf <liuzf1986@163.com>
2015-02-06 00:11:58 +0800

    implemtating TcpProxy

--- TcpProxy.hpp
+++ TcpProxy.hpp
@@ -3,7 +3,14 @@
 #include <stdint.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/epoll.h>
+#include <thread>
+#include <memory>
+#include <mutex>
 
+#include "InetSock.hpp"
+
+using namespace std;
 
 /**
  * User epoll for TcpProxy
@@ -13,4 +20,5 @@ class TcpProxy {
  public:
   
  private:
+  thread _looper;
 };
